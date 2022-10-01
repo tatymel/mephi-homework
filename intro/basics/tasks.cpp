51d80a94d8929e4d434e8afa48dce4569dcb5a2f
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-01 11:30:03 +0300

    test1

--- intro/basics/tasks.cpp
+++ intro/basics/tasks.cpp
@@ -1,38 +1,87 @@
-#include "tasks.h"
+#include <iostream>
 #include <cmath>
-
-int NOuter::NInner::DoSomething(int lhs, int rhs) {
+#include <limits>
+using namespace std;
+namespace NOuter{
+    namespace NInner{
+        int DoSomething(int a, int b){
+            return(a + b);
+        }
+    }
+    int DoSomething(int a, int b){
+        return abs(a - b);
+    }
 }
 
-int NOuter::DoSomething(int lhs, int rhs) {
+namespace NOverload{
+    int ProcessTwoArgs(int a, int b){
+        return a + b;
+    }
+    char ProcessTwoArgs(char a, char b){
+        return a > b ? a : b;
+    }
+    int ProcessTwoArgs(int a, char b){
+        return a - int(b);
+    }
 }
 
-int NOverload::ProcessTwoArgs(int lhs, int rhs) {
+namespace NLoop{
+    int SumInRange(const int a, const int b){
+        int sum = 0;
+        for(int i = a; i < b; i++){
+            sum += i;
+        }
+        return sum;
+    }
+    int CountFixedBitsInRange(const int a, const int b, const int k){
+        int ans = 0;
+        for(int i = a; i < b + 1; i++){
+            int s = 0;
+            int ii = i;
+            while (ii > 0){
+                s += (ii%2);
+                ii /= 2;
+            }
+            if (s == k){
+                ans += 1;
+            }
+
+        }
+        return ans;
+    }
 }
 
-char NOverload::ProcessTwoArgs(char lhs, char rhs) {
+namespace NMath{
+    bool IsPositive(int a){
+        if(a > 0)
+            return true;
+        return false;
+    }
+    double ComputeMathFormula(const double a){
+        return abs((sin(a) / 2 + cos(a)) * (sin(a) / 2 + cos(a)) + tan(a) * atan(a));
+    }
 }
 
-int NOverload::ProcessTwoArgs(int lhs, char rhs) {
+namespace NRecursion{
+    int CalculateFibonacci(const int a){
+        int fib1 = 1;
+        int fib2 = 1;
+        int t;
+        for(int i = 0; i < a - 2;i++){
+            t = fib1;
+            fib1 = fib2;
+            fib2 += t;
+        }
+        return fib2;
+    }
 }
 
-unsigned int NOverflow::WithOverflow(int lhs, int rhs) {
-}
+namespace NOverflow{
+    unsigned int WithOverflow(int a, int b){
+        return a + b;
+    }
+    uint64_t WithOverflow(int64_t a, int64_t b){
+        return a - b;
+    }
 
-uint64_t NOverflow::WithOverflow(int64_t lhs, int64_t rhs) {
-}
-
-int NLoop::SumInRange(const int lhs, const int rhs) {
-}
-
-int NLoop::CountFixedBitsInRange(const int from, const int to, const int bitsCnt) {
-}
-
-double NMath::ComputeMathFormula(const double arg) {
-}
-
-bool NMath::IsPositive(int arg) {
-}
-
-int NRecursion::CalculateFibonacci(const int arg) {
 }
