6f5ec1d1d3c392ff6357c83a5422fe66c0312514
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-26 20:27:03 +0300

    t1

--- advanced_containers/list_reverse/task.cpp
+++ advanced_containers/list_reverse/task.cpp
@@ -1,4 +1,10 @@
 #include "task.h"
-
+// 1 2 3 4 5
+// 5 4 3 2 1
 void ReverseList(std::list<int>& l) {
+    auto start_list = l;
+    for(int t : start_list){
+        l.push_front(t);
+        l.pop_back();
+    }
 }
