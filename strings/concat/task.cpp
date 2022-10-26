f830dd97aef878562ba0e44dbe3ce5566f60af1b
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-26 17:03:27 +0300

    t1

--- strings/concat/task.cpp
+++ strings/concat/task.cpp
@@ -1,4 +1,9 @@
 #include "task.h"
+#include "string_view"
 
 char* concat(const char* lhs, const char* rhs) {
+    char* new_str = "";
+    strcat(new_str, lhs);
+    strcat(new_str, rhs);
+    return new_str;
 }
