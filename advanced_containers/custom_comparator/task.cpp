64bdef1c499d197eb21e31db82d26813bfaf519e
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-26 21:01:03 +0300

    rt1

--- advanced_containers/custom_comparator/task.cpp
+++ advanced_containers/custom_comparator/task.cpp
@@ -1,10 +1,26 @@
 #include "task.h"
 
 bool operator <(const SuperKey& lhs, const SuperKey& rhs) {
+    if(lhs.StrPart > rhs.StrPart)
+        return false;
+    if(lhs.StrPart < rhs.StrPart)
+        return true;
+    if(lhs.IntPart > rhs.IntPart)
+        return false;
+    if(lhs.IntPart < rhs.IntPart)
+        return true;
+    if(lhs.FloatPart > rhs.FloatPart)
+        return false;
+    if(lhs.FloatPart < rhs.FloatPart)
+        return true;
+    return false;
 }
 
 void PopulateMap(
     std::map<SuperKey, std::string>& map,
     const std::vector<std::pair<SuperKey, std::string>>& toAdd
 ) {
+    for(auto el : toAdd){
+        map.insert(el);
+    }
 }
