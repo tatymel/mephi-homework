43aa61b0f00c558d7975a21699cea2522f971a4a
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-26 17:15:49 +0300

    r2

--- strings/concat/task.cpp
+++ strings/concat/task.cpp
@@ -2,7 +2,8 @@
 #include "string_view"
 
 char* concat(const char* lhs, const char* rhs) {
-    char* new_str = "";
+     char* new_str = const_cast<char*>(lhs);
+//c<char*>(new_str);
     strcat(new_str, lhs);
     strcat(new_str, rhs);
     return new_str;
