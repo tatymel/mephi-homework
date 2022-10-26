c3e84bec6876f4bb1e88d6cc32b44f572cc03c63
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-26 17:34:35 +0300

    t11

--- strings/concat/task.cpp
+++ strings/concat/task.cpp
@@ -2,16 +2,18 @@
 #include "string_view"
 
 char* concat(const char* lhs, const char* rhs) {
-     char* new_str = const_cast<char*>(lhs);
-int i = 0;
-while (lhs[i] != '\0'){
-    new_str += lhs[i];
-    i++;
-}
-i = 0;
-while(rhs[i] != '\0'){
-    new_str += rhs[i];
-    i++;
-}
+    char* new_str = new char;
+
+    int i = 0;
+    while (lhs[i] != '\0'){
+        new_str[i] = lhs[i];
+        i++;
+    }
+    int n = i;
+    while(rhs[i - n] != '\0'){
+        new_str[i] = rhs[i - n];
+        i++;
+    }
+    cout << lhs;
     return new_str;
 }
