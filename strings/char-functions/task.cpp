563f72357f05661c17e96f92b989b0c9c44f4e8b
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-27 02:10:04 +0300

    fixq

--- strings/char-functions/task.cpp
+++ strings/char-functions/task.cpp
@@ -1,4 +1,9 @@
+#include <cstring>
 bool isalpha(unsigned char c) {
+    const char *alf = "qwertyuioplkjhgfdsazxcvbnmQWERTYUIOPLKJHGFDSAZXCVBNM";
+    if(std::strchr(alf, c) != nullptr)
+        return true;
+    return false;
 }
 
 unsigned char tolower(unsigned char c) {
