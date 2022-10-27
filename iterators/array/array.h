93e2f541e745b98296d6c321177cca11702780ba
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-27 19:48:02 +0300

    t1

--- iterators/array/array.h
+++ iterators/array/array.h
@@ -227,9 +227,9 @@ std::ostream& operator <<(std::ostream& ostream, const Array<T>& array) {
         ostream << "Result Array's capacity is " << array.Capacity() << " and size is " << array.Size()
                 << ", elements are: ";
         for (size_t i = 0; i < array.Size() - 1; ++i){
-            ostream << *(array.Elements_ + i) << ", ";
+            ostream << array.Elements_[i] << ", ";
         }
-        ostream << *(array.Elements_ + array.Size() - 1);
+        ostream << array.Elements_[array.Size() - 1];
     }
     return ostream;
 }
