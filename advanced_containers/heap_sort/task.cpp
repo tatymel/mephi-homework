bf65850319d1666d4b27f882fc65e0ea8dfba555
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-27 21:48:24 +0300

    t1

--- advanced_containers/heap_sort/task.cpp
+++ advanced_containers/heap_sort/task.cpp
@@ -3,4 +3,14 @@
 using namespace std;
 
 vector<int> HeapSort(const vector<int>& v) {
+    priority_queue<int> pr;
+    vector<int> nv(v.size(), 0);
+    for(int t : v)
+        pr.push(t);
+
+    for(int i = v.size() - 1; i > -1; i--){
+        nv[i] = pr.top();
+        pr.pop();
+    }
+    return nv;
 }
