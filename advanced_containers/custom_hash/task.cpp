a7e3a05d5e4f784c773074854a0d9b43f5ae6842
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-10-29 02:29:04 +0300

    tt

--- advanced_containers/custom_hash/task.cpp
+++ advanced_containers/custom_hash/task.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 void PopulateHashMap(
-    unordered_map<SuperKey, string>& hashMap,
-    const vector<pair<SuperKey, string>>& toAdd
+        unordered_map<SuperKey, string>& hashMap,
+        const vector<pair<SuperKey, string>>& toAdd
 ) {
+    for(auto el : toAdd)
+        hashMap[el.first] = el.second;
 }
--- advanced_containers/custom_hash/task.h
+++ advanced_containers/custom_hash/task.h
@@ -11,13 +11,39 @@ struct SuperKey {
         return this->StrPart == rhs.StrPart && this->IntPart == rhs.IntPart && this->FloatPart == rhs.FloatPart;
     }
 };
-
-/*
- * Напишите хэш-функцию, реализовав специализацию шаблона std::hash для типа SuperKey
- * Напишите функцию PopulateHashMap, которая добавляет в hashMap пары ключ-значение из данного вектора toAdd
- */
+namespace std{
+    template<>
+    struct hash<SuperKey>{
+        size_t operator()(const SuperKey& sk) const{
+            size_t h1 = hash<string>{}(sk.StrPart);
+            size_t h2 = hash<int>{}(sk.IntPart);
+            size_t h3 = hash<float>{}(sk.FloatPart);
+            return h1 ^ h2 ^ (h3 << 1);
+        }
+    };
+}
 void PopulateHashMap(
-    std::unordered_map<SuperKey, std::string>& hashMap,
-    const std::vector<std::pair<SuperKey, std::string>>& toAdd
+        std::unordered_map<SuperKey, std::string>& hashMap,
+        const std::vector<std::pair<SuperKey, std::string>>& toAdd
 );
 
+/*
+namespace std{
+
+    template<>
+    struct hash<SuperKey>{
+        std::size_t operator()(const SuperKey& Skey) const
+        {
+            using std::size_t;
+            using std::hash;
+            using std::string;
+
+            return ((hash<std::string>()(Skey.StrPart)
+                     ^ (hash<int>()(Skey.IntPart) << 1)) >> 1)
+                   ^ (hash<float>()(Skey.FloatPart) << 1);
+        }
+
+    };
+
+
+}*/
