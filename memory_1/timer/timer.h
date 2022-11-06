911e4c7ac25a42ffd661ebe2b9d7349bbd6e847e
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-11-06 16:32:56 +0300

    t1

--- memory_1/timer/timer.h
+++ memory_1/timer/timer.h
@@ -1,7 +1,33 @@
+#include <chrono>
+#include <string>
+#include <sstream>
 class Timer {
+private:
+    std::chrono::seconds sec;
+    std::chrono::steady_clock::time_point  begin;
 public:
-    bool Expired() const;
+    Timer(std::chrono::seconds sec) : sec(sec) {
+        begin = std::chrono::steady_clock::now();
+    }
+
+    bool Expired() const{
+        while(std::chrono::steady_clock::now() - begin <= sec){
+            return false;
+        }
+        return true;
+    }
 };
 
+
 class TimeMeasurer {
+private:
+    std::stringstream& Ostream_;
+    std::chrono::steady_clock::time_point begin;
+public:
+    TimeMeasurer(std::stringstream& out) : Ostream_(out){
+        begin = std::chrono::steady_clock::now();
+    }
+    ~TimeMeasurer(){
+        Ostream_ << "Elapsed time: " << std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - begin).count() << std::endl;
+    }
 };
