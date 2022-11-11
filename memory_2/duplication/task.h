639f894763e0f3192acbf5607de64e9c28436407
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-11-12 02:11:35 +0300

    t1

---
+++ memory_2/duplication/task.h
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+std::vector<std::unique_ptr<std::string>> Duplicate(const std::vector<std::shared_ptr<std::string>>& in){
+    std::vector<std::unique_ptr<std::string>> out;
+    for(size_t i = 0; i < in.size(); ++i){
+        out.push_back(std::make_unique<std::string>(*in[i]));
+    }
+    return out;
+}
+std::vector<std::shared_ptr<std::string>> DeDuplicate(std::vector<std::unique_ptr<std::string>>& in){
+    std::vector<std::shared_ptr<std::string>> out(in.size());
+
+    for(size_t i = 0; i < in.size(); ++i){
+        bool fl = true;
+        for(size_t j = 0; j < std::min(i, out.size()); ++j){
+            if(*in[i] == *out[j]){
+                auto sp_new= out[j];
+                out[i] = sp_new;
+                fl = false;
+                break;
+            }
+        }
+        if(fl){
+            out[i] = (std::make_shared<std::string>(*in[i]));
+        }
+    }
+    return out;
+}
