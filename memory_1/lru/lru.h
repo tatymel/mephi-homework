7f1d07990194eeefc576ed5991a34706062968f1
Татьяна Мельник <tmelnik@MacBook-Air-Tatana.local>
2022-11-11 22:32:10 +0300

    23r

--- memory_1/lru/lru.h
+++ memory_1/lru/lru.h
@@ -1,13 +1,13 @@
 #include <iostream>
 #include <unordered_map>
 #include <list>
-template <typename K, typename V>
+template<typename K, typename V>
 class LruCache {
 private:
     size_t Max_size_;
     size_t Temp_size_;
-    std::unordered_map<K, typename std::list<V>::iterator> mp;
-    std::list<V> Elements_;
+    std::unordered_map<K, typename std::list<std::pair<K, V>>::iterator> mp;
+    std::list<std::pair<K, V>> Elements_;
 public:
     LruCache(size_t max_size) : Max_size_(max_size)
     {
@@ -20,18 +20,19 @@ public:
             if(Temp_size_ < Max_size_){
                 ++Temp_size_;
             } else {
-                for (auto it = mp.rbegin(); it != mp.rend(); --it) {
+                /*for (auto it = mp.begin(); it != mp.end(); ++it) {
                     if (it->second == --Elements_.end()) {
                         mp.erase(it);
                         break;
                     }
-                }
+                }*/
+                mp.erase(Elements_.back().first);
                 Elements_.pop_back();
             }
         }else {
             Elements_.erase(mp[key]);
         }
-        Elements_.push_front(value);
+        Elements_.push_front({key, value});
         mp[key] = Elements_.begin();
     }
 
@@ -42,9 +43,10 @@ public:
             return false;
         else
         {
-            *value = *(it->second);
-            Elements_.push_front(*it->second);
+            *value = (it->second->second);
+            Elements_.push_front({key, (it->second->second)});
             Elements_.erase(it->second);
+            mp.erase(key);
             mp[key] = Elements_.begin();
             return true;
         }
